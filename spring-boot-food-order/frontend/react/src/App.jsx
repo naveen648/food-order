5d5d4235829d619e531049b5bf11e82a96432fb7
Naveen Kumar N P <naveenkumarnp404@gmail.com>
2024-10-12 09:07:05 +0530

    jwt implementation

---
+++ spring-boot-food-order/frontend/react/src/App.jsx
@@ -0,0 +1,8 @@
+
+
+function App() {
+
+    return <h1>food portal in progress...</h1>
+}
+
+export default App
---
+++ spring-boot-food-order/src/main/java/com/naveen/SpringBootFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.naveen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFoodOrderApplication.class, args);
+	}
+
+}
